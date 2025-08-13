8a6dc42779603e6d1e102b699912e70aa7082f65
Srikanth Bhimineni <srikanthbhimineni@Srikanths-MacBook-Air.attlocal.net>
2025-08-13 16:21:39 -0500

    added hello

--- Example1/src/Example1.java
+++ Example1/src/Example1.java
@@ -5,6 +5,7 @@ public class Example1 {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        
+        System.out.printf("Hello");
+
     }
 }
